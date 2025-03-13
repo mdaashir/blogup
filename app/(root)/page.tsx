5600620837b4cd92dc94df8591ccc2343ceb8685
mdaashir <22pt17@psgtech.ac.in>
2025-03-13 18:51:41 +0530

    update Home component to use Work Sans font and enhance global styles with new utility classes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1 className="heading">Home</h1>
+            <h1 className="heading font-work-sans">Home</h1>
         </div>
     )
 }
